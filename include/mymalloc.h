9fb5e4317efdb0e773da7a8660fcedfcae6e3299
root <andrewflangas@yahoo.com>
2022-08-28 19:33:06 -0500

    mend

--- include/mymalloc.h
+++ include/mymalloc.h
@@ -1,6 +1,25 @@
 #ifndef LAB1_LIBRARY_H
 #define LAB1_LIBRARY_H
 
-void hello(void);
+#include <sys/mman.h>
+#include <stdio.h>
+
+typedef struct header_t
+{
+    int size;
+    int magic;
+} header_t;
+
+typedef struct node_t
+{
+    int size;
+    struct node_t *next;
+} node_t;
+
+void InitializeFreeList();
+
+void Free(void *ptr);
+
+void *Malloc(size_t size);
 
 #endif //LAB1_LIBRARY_H
--- src/mymalloc.c
+++ src/mymalloc.c
@@ -1,7 +1,18 @@
 #include "mymalloc.h"
 
-#include <stdio.h>
+void InitializeFreeList()
+{
+    node_t *head = mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+    head->size = 4096 - sizeof(node_t);
+    head->next = NULL;
+}
 
-void hello(void) {
-    printf("Hello, World!\n");
+void Free(void *ptr)
+{
+    header_t *hptr = (header_t *) ptr -1;
+}
+
+void *Malloc(size_t size)
+{
+
 }
