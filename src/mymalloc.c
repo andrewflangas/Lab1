dd5dad8c218d432a249f073ccd6a93266734bbb5
root <andrewflangas@yahoo.com>
2022-09-09 00:21:39 -0500

    first-fit implementation complete

--- src/mymalloc.c
+++ src/mymalloc.c
@@ -13,34 +13,41 @@ void Free(void *ptr)
     }
 }
 
-void *First_Fit(size_t size, size_t *block_size)
+void *First_Fit(size_t size, node_t *block)
 {
-    for(int i=0; i<MEMORY_SIZE; ++i)
+    node_t *allocation = block;
+    for(int i=0; i<block->size; ++i)
     {
         for(int j=0; j<size; ++j)
         {
-            if(block_size[j] >= )
+            if(sizeof(block[i]) >= size)
+            {
+                block[j].size -= size;
+                allocation[j] = block[j];
+                break;
+            }
         }
-
     }
+    return (void*) (&allocation[1]);
 }
 
 void *Malloc(size_t size)
 {
     node_t *head;
-    size_t *blk_size;
+    node_t *mem_blk;
     size_t prev_size;
     if(!flag)
     {
-        blk_size = mmap(NULL, MEMORY_SIZE, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-        head->size = MEMORY_SIZE + sizeof(node_t);
-        head->next = NULL;
+        mem_blk = mmap(NULL, MEMORY_SIZE, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+        mem_blk->size = MEMORY_SIZE + sizeof(node_t);
+        mem_blk->next = NULL;
         flag = true;
     }
 
-    prev_size = head->size;
-    head = First_Fit(size,blk_size);
+    prev_size = mem_blk->size;
+    head = First_Fit(size,mem_blk);
     head->size = prev_size - (size-8);
+    head->next = NULL;
 
     return (void*) (&head[1]);
 }
