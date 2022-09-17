9412701dbb481fe37c622d71b368c7fee8d56e63
root <andrewflangas@yahoo.com>
2022-09-16 22:07:56 -0500

    mend

---
+++ build/CMakeFiles/Lab1.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/OperatingSystems/Lab1/src/main.c" "CMakeFiles/Lab1.dir/src/main.c.o" "gcc" "CMakeFiles/Lab1.dir/src/main.c.o.d"
+  "/OperatingSystems/Lab1/src/mymalloc.c" "CMakeFiles/Lab1.dir/src/mymalloc.c.o" "gcc" "CMakeFiles/Lab1.dir/src/mymalloc.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Lab1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab1.dir/src/main.c.o"
+  "CMakeFiles/Lab1.dir/src/main.c.o.d"
+  "CMakeFiles/Lab1.dir/src/mymalloc.c.o"
+  "CMakeFiles/Lab1.dir/src/mymalloc.c.o.d"
+  "Lab1"
+  "Lab1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
